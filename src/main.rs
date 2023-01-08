1b287e955ea002e01dc0bc75e9885784c82a365a
Greenlink <lennon@sistemasbr.net>
2023-01-07 23:37:20 -0300

    modify: make a simble page with yew.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use yew::prelude::*;
+
+#[function_component(Producer)]
+fn producer() -> Html {
+    html!(
+        <div>
+            <h3>{"Producer"}</h3>
+        </div>
+    )
+}
+
+#[function_component(Consumer)]
+fn consumer() -> Html {
+    html!(
+        <div>
+            <h3>{"Consumer"}</h3>
+        </div>
+    )
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    html!(
+        <div>
+            <Producer/>
+            <Consumer/>
+        </div>
+    )
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
